aeca0ae945ac4f45e5d622d6c6aeeba11f6ddd80
Davisson <dvisson.medeiros@gmail.com>
2017-05-27 16:15:36 -0300

    Renomeia package

--- src/main/java/com/dvsmedeiros/group/api/app/GroupApiApplication.java
+++ src/main/java/com/dvsmedeiros/group/api/app/GroupApiApplication.java
@@ -1,4 +1,4 @@
-package com.dvsmedeiros.group.api;
+package com.dvsmedeiros.group.api.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
