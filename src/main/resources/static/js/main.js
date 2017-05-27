81c8b2d356bebe942602ae093be4fc8efa13da69
Davisson <dvisson.medeiros@gmail.com>
2017-05-27 18:02:36 -0300

    add routes

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,13 +2,31 @@ angular.module('app', ['ngRoute', 'ngAnimate'])
 	.config(function($routeProvider, $locationProvider, $httpProvider) {
 
 		//$locationProvider.html5Mode(true);
-
+		
 		//HOME
-		$routeProvider.when('/home', {
-			templateUrl: 'home.html',
-			controller: 'HomeController'
+		$routeProvider.when('/', {
+			templateUrl: 'groups.html',
+			controller: 'GroupsController'
 		});
-
+		
+		//GROUPS
+		$routeProvider.when('/groups', {
+			templateUrl: 'groups.html',
+			controller: 'GroupsController'
+		});
+		
+		//GROUP
+		$routeProvider.when('/group/:groupId', {
+			templateUrl: 'group.html',
+			controller: 'GroupController'
+		});
+		
+		//PROFILE
+		$routeProvider.when('/profile', {
+			templateUrl: 'profile.html',
+			controller: 'ProfileController'
+		});
+		
 		$routeProvider.otherwise({redirectTo: '/'});		
 
 		$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
