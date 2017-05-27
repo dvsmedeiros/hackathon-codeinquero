efa255befc80d899bb60acbecfd6dd1d6d73b1ce
Davisson <dvisson.medeiros@gmail.com>
2017-05-27 16:00:35 -0300

    commit inicial

---
+++ src/main/java/com/dvsmedeiros/group/api/GroupApiApplication.java
@@ -0,0 +1,20 @@
+package com.dvsmedeiros.group.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+import com.dvsmedeiros.bce.core.BceCoreApplication;
+
+@Import(BceCoreApplication.class)
+@ComponentScan("com.dvsmedeiros")
+@EnableAutoConfiguration
+@SpringBootApplication
+public class GroupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupApiApplication.class, args);
+	}
+}
